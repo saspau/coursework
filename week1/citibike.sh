26b1c81160bf74871ebfe5b7dafe6d8907ceb84a
sasha <31082004+saspau@users.noreply.github.com>
2018-06-12 16:05:50 -0400

    added solutions

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -5,8 +5,8 @@
 
 # count the number of unique stations
 cut -d, -f4 201402-citibike-tripdata.csv | tail -n +2 | sort | uniq -c | wc -l
-#330
 cut -d -f8 201402-citibike-tripdata.csv | tail -n +2 | sort | uniq -c | wc -l
+# checking the number of unique start and end stations; they match!
 
 # count the number of unique bikes
 cut -d, -f12 201402-citibike-tripdata.csv | tail -n +2 | sort | uniq -c | wc -l
@@ -24,8 +24,10 @@ cut -d, -f2 201402-citibike-tripdata.csv | tail -n +2 | sort | cut -d ' ' -f1 | 
 cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | sort -rn | head -n1
 
 # count the number of rides by gender and birth year
-
+cut -d, -f14,15 201402-citibike-tripdata.csv | sort | uniq -c| sort -rn
 
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+cut -d, -f5 201402-citibike-tripdata.csv | tail -n +2 | grep '.*[0-9].*&.*[0-9].*' | wc -l
 
 # compute the average trip duration
+cut -d, -f1 201402-citibike-tripdata.csv | tail -n +2 | tr '"' ' ' | awk '{sum += $1; trips++} END {print sum/trips}'
